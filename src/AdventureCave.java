0d73ce4219b91b70659c479fb1f1971e276886c4
Taranveer Pabla <taran12315@gmail.com>
2020-11-11 21:14:29 -0500

    create levelUp

--- src/AdventureCave.java
+++ src/AdventureCave.java
@@ -1,6 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
+/*
+if(playerHealth <= 0){
+       System.out.println("Oh no, " + playerName +" died! They were level " + playerLevel);
+       System.exit(0);
+   }
+*/
+
 public class AdventureCave {
     public static void main(String[] args) {
         Random randomGenerator = new Random();
@@ -18,9 +26,10 @@ public class AdventureCave {
         System.out.println( "What is your name?");
         playerName = playerInput.nextLine();
 
+
         System.out.println( "You are " + playerName + ", a level " + playerLevel + " adventurer.");
         System.out.println("You made it to the entrance of the cave.");
-        playerLevel = playerLevel + 1;
+        playerLevel += 1;
         System.out.println("You leveled up. You are now level " + playerLevel);
 
         //_________End of Level 1_________
@@ -36,7 +45,9 @@ public class AdventureCave {
             System.exit(0);
         }
 
+        // _________Start of Level 2_________
 
+        //Room 2.1 Description
         System.out.println("You are in a cave." + "\nThere is a dark path to the left." + "\nThere is a pile of rubble to the right." + "\nWould you like to go 'left' or 'right'?");
 
         playerAnswer = playerInput.nextLine();    // get player's answer
@@ -49,68 +60,59 @@ public class AdventureCave {
         }
         else if(playerAnswer.equalsIgnoreCase("left")){
             System.out.println("The path gets very dark. Another cunning rock ambushes you.");
-            rockDamage = 105.0;
+            rockDamage = 5.0;
             playerHealth -= rockDamage;
             System.out.println("You took " + rockDamage + " damage. You have " + playerHealth + " health remaining.");
-            // Check if damage killed the player
-            if(playerHealth <= 0){
-                //System.out.println("Oh no, " + playerName +" died! They were level " + playerLevel);
-                //System.exit(0);
-                checkHealth(playerName, playerLevel);
-            }
+            checkHealth(playerHealth, playerName, playerLevel);
         }
 
-        // Room 2 Description
+        // Room 2.2 Description
         System.out.println("There is a small slime here. It burbles at you menacingly." + "\nWould you like to 'befriend' it, 'fight' it or 'run' away?" );
         playerAnswer = playerInput.nextLine();  // get player's answer
 
-        /* If Statements
-         * For choice 2
-         */
         if(playerAnswer.equalsIgnoreCase("befriend")){
-            System.out.println("You say hello to the slime and offer it some of your food.\n"
-                    + "The slime dissolves your food. It burbles less menacingly and leads you to a magic hat."
-                    + "\nYou found a magic hat!");
-            // Player found some treasure - level up!
-            playerLevel = playerLevel + 1;
+            System.out.println("You say hello to the slime and offer it some of your food.\n" + "The slime dissolves your food. It burbles less menacingly and leads you to a magic hat." + "\nYou found a magic hat!");
+
+            playerLevel += 1;
             System.out.println("You leveled up. You are now level " + playerLevel);
         }
         else if(playerAnswer.equalsIgnoreCase("fight")){
-            System.out.println("You hit the slime with your sword and it dissolves.\n"
-                    + "The slime burbles angrily and sprays some acid at you.");
-            playerLevel = playerLevel + 1;
+            System.out.println("You hit the slime with your sword and it dissolves.\n" + "The slime burbles angrily and sprays some acid at you.");
+
             rockDamage = 5.0;
             playerHealth -= rockDamage;
-            System.out.println("You took " + rockDamage
-                    + " damage. You have " + playerHealth
-                    + " health remaining.");
-            // Check if damage killed the player
-            if( playerHealth <= 0 ){
-                System.out.println("Oh no, "
-                        + playerName +" died! They were level "
-                        + playerLevel);
-                System.exit(0);
-            }
+            System.out.println("You took " + rockDamage + " damage. You have " + playerHealth + " health remaining.");
+            checkHealth(playerHealth, playerName, playerLevel);
+
+            playerLevel += 1;
+            System.out.println("You leveled up. You are now level " + playerLevel);
         }
         else if(playerAnswer.equalsIgnoreCase("run")){
-            System.out.println("You turn tail and run. The slime does not pursue you.\n"
-                    + "You fall into a healing spring while running.");
+            System.out.println("You turn tail and run. The slime does not pursue you.\n" + "You fall into a healing spring while running.");
             double healing = 3.5; // Heal the player
             playerHealth += healing;
             System.out.println("You were healed for " + healing + ". Your health is now " + playerHealth);
-            playerLevel = playerLevel + 1;
+
+            //playerLevel += 1;
+            //System.out.println("You leveled up. You are now level " + playerLevel);
+            levelUp(playerLevel);
         }
 
-        System.out.println(playerName +" survived the Cave of Wonders! You made it to level "+ playerLevel);
-
-
+        System.out.println(playerName + " survived the Cave of Wonders! You made it to level "+ playerLevel);
         playerInput.close();
     }
 
 
-    public static void checkHealth(String name, Integer level) {
-        System.out.println("Oh no, " + name +" died! They were level " + level);
-        System.exit(0);
+    public static void checkHealth(double health, String name, int level) {
+        if(health <= 0) {
+            System.out.println("Oh no, " + name + " died! They were level " + level);
+            System.exit(0);
+        }
+    }
+
+    public static void levelUp(int level) {
+        level += 1;
+        System.out.println("You leveled up. You are now level " + level);
     }
 
 }
